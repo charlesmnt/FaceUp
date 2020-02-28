3e96990977ee69185837aa3f7f03b555e648ec95
Noel Paganelli <noel@lacapsule.academy>
2020-02-28 11:48:50 +0100

    project init

---
+++ screens/GalleryScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import {Card, Badge, Text} from 'react-native-elements';
+
+import {connect} from 'react-redux';
+
+function GalleryScreen(props) {
+
+  let cardList = props.pictureUrlList.map((url, i)=> {
+      return <Card 
+        image={{uri: url}}>
+
+          <Badge status="success" value="homme"/>
+          <Badge status="success" value="70 ans"/>
+          <Badge status="success" value="barbe"/>
+          <Badge status="success" value="joyeux !"/>
+          <Badge status="success" value="cheveux gris"/>
+      </Card>
+  }
+  );
+
+  return (
+    <ScrollView style={{marginTop: 25}}>
+      
+      <Text h4 style={{textAlign: 'center'}}>John's Gallery</Text>
+
+      {cardList}
+    
+    </ScrollView>
+  );
+}
+
+function mapStateToProps(state) {
+  return { pictureUrlList : state.pictureUrlList }
+}
+
+export default connect(
+  mapStateToProps, 
+  null
+)(GalleryScreen);
