8e3a759473f5fcca8f0b7584af80824622b25d2a
Charles M <charle.monot@gmail.com>
2022-03-16 10:17:56 +0100

    version finale back-end

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 var uniqid = require('uniqid');
 var fs = require('fs');
 
+var request = require('sync-request');
+
 var cloudinary = require('cloudinary').v2;
 cloudinary.config({ 
-  cloud_name: 'dvqjak***', 
-  api_key: '767287626552***', 
-  api_secret: 'BRfbaQzy3xSWMq0dNqdLAS***' 
+  cloud_name: 'drajlbdf4', 
+  api_key: '622945721282649', 
+  api_secret: 'g_YM9aF1-LoEqPEL_tIYmOUnRVg' 
 });
 
 /* GET home page. */
@@ -18,10 +20,25 @@ router.get('/', function(req, res, next) {
 router.post('/upload', async function(req, res, next) {
   
   var pictureName = './tmp/'+uniqid()+'.jpg';
+  
+
   var resultCopy = await req.files.avatar.mv(pictureName);
   if(!resultCopy) {
     var resultCloudinary = await cloudinary.uploader.upload(pictureName);
-    res.json(resultCloudinary);
+    
+    var options = {
+      json: {
+        apiKey: "5c0a5d392c1745d2ae84dc0b1483bfd2",
+        image: resultCloudinary.url,
+      },
+    };
+
+    var resultDetectionRaw = await request('POST', 'https://lacapsule-faceapi.herokuapp.com/api/detect', options); 
+   
+    var resultDetection = await resultDetectionRaw.body;
+    resultDetection = await JSON.parse(resultDetection);
+
+    res.json({resultCloudinary, resultDetection});
   } else {
     res.json({error: resultCopy});
   }
@@ -29,5 +46,7 @@ router.post('/upload', async function(req, res, next) {
   fs.unlinkSync(pictureName);
   
 });
+
+
 
 module.exports = router;
